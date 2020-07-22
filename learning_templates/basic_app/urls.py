0b877ea54142cfd153c35c4ae1f0413e20d3140f
Mateo Laresgoiti <Mateo@mateos-macbook.attlocal.net>
2020-07-22 18:05:35 -0400

    first

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('registration/',views.register,name='registration'),
+    path('user_login/',views.user_login,name='user_login'),
+    path('relative/',views.relative,name='relative'),
+    path('other/',views.other,name='other'),
+]
